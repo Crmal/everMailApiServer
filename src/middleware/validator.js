57615119d4ba1bf84c9f4bd33c86f148512559b9
Crmal <pjm2207@naver.com>
2022-05-02 03:19:05 +0900

    feat: signValidaotr 추가

---
+++ src/middleware/validator.js
@@ -0,0 +1,56 @@
+const regEmail =
+  /^[0-9a-zA-Z-_]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*\.[a-zA-Z]{2,3}$/i;
+const regPassword = /^(?=.*[A-Za-z])(?=.*\d)(?=.*[$@$!%*#?&])[A-Za-z\d$@$!%*#?&]{8,20}$/;
+const regPhone = /^[0-9]{11,11}$/;
+const regName = /^[ㄱ-ㅎ|가-힣]{2,5}$/;
+
+export function signValidator(req, res, next) {
+  // eslint-disable-next-line prefer-destructuring
+  const { name, nickName, password, email, phone } = req.body;
+  if (!name || !nickName || !password || !email || !phone) {
+    return res.status(400).json({
+      error: {
+        message: '회원가입 형식이 올바르지 않습니다.',
+      },
+    });
+  }
+  if (!regEmail.test(email) && email.length > 30) {
+    return res.status(400).json({
+      error: {
+        message: '이메일 형식이 올바르지 않습니다.',
+      },
+    });
+  }
+  // 8~20자리 이내, 비밀번호는 공백 없이, 영문,숫자,특수문자 혼합
+  // TODO: 각각의 케이스에 대해 에러메세지를 추가해야 함
+  if (!regPassword.test(password)) {
+    return res.status(400).json({
+      error: {
+        message: '비밀번호는 공백 없이, 영문,숫자,특수문자 혼합한 8~20자로 입력해주세요',
+      },
+    });
+  }
+  // 숫자로만 이루어져있는지 11글자인지
+  if (!regPhone.test(phone)) {
+    return res.status(400).json({
+      error: {
+        message: 'phone는 -를 제외한 11자를 입력해주세요.',
+      },
+    });
+  }
+  if (!regName.test(name)) {
+    return res.status(400).json({
+      error: {
+        message: 'name은 한글로 5글자 이하를 입력해주세요.',
+      },
+    });
+  }
+  if (nickName.length > 10) {
+    return res.status(400).json({
+      error: {
+        message: 'nickName는 10자이하를 입력해주세요.',
+      },
+    });
+  }
+  return next();
+}
