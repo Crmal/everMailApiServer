7c454529a237246f37229f82445d43d8d9d5d517
Crmal <pjm2207@naver.com>
2022-05-02 04:31:39 +0900

    feat: jwt checker 추가

---
+++ src/middleware/checker.js
@@ -0,0 +1,34 @@
+import { verify } from 'jsonwebtoken';
+
+export const loginChecker = (req, res, next) => {
+  const token = req.header('x-access-token');
+
+  if (!token) {
+    return res.status(401).json({
+      error: {
+        message: '로그인을 먼저 해주세요.',
+      },
+    });
+  }
+
+  try {
+    verify(token, process.env.JWT_SECRET);
+  } catch (e) {
+    if (e.name === 'JsonWebTokenError') {
+      return res.status(401).json({
+        error: {
+          message: '토큰 정보가 잘못되었습니다. 다시 시도해주세요.',
+        },
+      });
+    }
+    if (e.name === 'TokenExpiredError') {
+      return res.status(403).json({
+        error: {
+          message: '토큰이 만료되었습니다. 다시 로그인해주세요.',
+        },
+      });
+    }
+  }
+
+  return next();
+};
