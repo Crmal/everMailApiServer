45de3c56be82f9785e6eb3bf818a0af843e3a95f
Crmal <pjm2207@naver.com>
2022-05-02 03:10:02 +0900

    feat: sign-up 추가

---
+++ src/api/auth/index.js
@@ -0,0 +1,35 @@
+import bcrypt from 'bcrypt';
+import { Router } from 'express';
+import { sign } from 'jsonwebtoken';
+
+import User from '../../models/user';
+
+const auth = Router();
+
+auth.post('/sign-up', async (req, res) => {
+  // eslint-disable-next-line
+  const { name, id_name, password, email, phone } = req.body;
+  const hash = await bcrypt.hash(password, 10);
+  // eslint-disable-next-line camelcase
+  const userData = await User.findOne({ where: { id_name } });
+  if (userData) {
+    return res.status(409).json({
+      error: {
+        message: '이미 가입된 이메일입니다. 로그인을 진행해주세요.',
+      },
+    });
+  }
+  await User.create({
+    name,
+    // eslint-disable-next-line camelcase
+    id_name,
+    password: hash,
+    email,
+    phone,
+  });
+  return res.status(200).json({
+    data: {
+      message: '회원가입이 완료되었습니다.',
+    },
+  });
+});
