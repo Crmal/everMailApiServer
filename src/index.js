ad04608470556eecd3a317697b5dcf8f81e7ac66
Crmal <pjm2207@naver.com>
2022-05-06 01:37:18 +0900

    fix: index.js 라우터 설정 db라우터 설정

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import { config } from 'dotenv';
 import express, { json } from 'express';
 
+import apiRouter from './api';
 import sequelize from './models';
 
 const app = express();
@@ -12,9 +13,10 @@ app.use(json());
 app.get('/', async (req, res) => {
   res.send('everymail');
 });
+app.use('/api', apiRouter);
 // 디비 연결
 sequelize.sequelize // 시퀄라이즈 연결
-  .sync({ force: true }) // force는 서버 실행시마다 테이블 재생성 여부
+  .sync({ force: false }) // force는 서버 실행시마다 테이블 재생성 여부
   .then(() => {
     // eslint-disable-next-line no-console
     console.log('데이터베이스 연결 성공');
