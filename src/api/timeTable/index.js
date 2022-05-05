b032a656123a43e0d814e88527dc9beae46dd33b
Crmal <pjm2207@naver.com>
2022-05-06 02:02:38 +0900

    fix: switch를 객체 프로퍼티로 변경

--- src/api/timeTable/index.js
+++ src/api/timeTable/index.js
@@ -56,54 +56,44 @@ timeTable.post('/', async (req, res) => {
     raw: true,
   });
   const nullCount = Object.values(userTimeTables).indexOf(null);
-  // eslint-disable-next-line default-case
-  switch (Object.keys(userTimeTables)[nullCount]) {
-    case 'one_id':
-      // eslint-disable-next-line no-await-in-loop
-      await UserTimeTable.update({ one_id: id }, { where: { user_id: userData.id } });
-      break;
-    case 'two_id':
-      // eslint-disable-next-line no-await-in-loop
-      await UserTimeTable.update({ two_id: id }, { where: { user_id: userData.id } });
-      break;
-    case 'three_id':
-      // eslint-disable-next-line no-await-in-loop
-      await UserTimeTable.update({ three_id: id }, { where: { user_id: userData.id } });
-      break;
-    case 'four_id':
-      // eslint-disable-next-line no-await-in-loop
-      await UserTimeTable.update({ four_id: id }, { where: { user_id: userData.id } });
-      break;
-    case 'five_id':
-      // eslint-disable-next-line no-await-in-loop
-      await UserTimeTable.update({ five_id: id }, { where: { user_id: userData.id } });
-      break;
-    case 'six_id':
-      // eslint-disable-next-line no-await-in-loop
-      await UserTimeTable.update({ six_id: id }, { where: { user_id: userData.id } });
-      break;
-    case 'seven_id':
-      // eslint-disable-next-line no-await-in-loop
-      await UserTimeTable.update({ seven_id: id }, { where: { user_id: userData.id } });
-      break;
-    case 'eight_id':
-      // eslint-disable-next-line no-await-in-loop
-      await UserTimeTable.update({ eight_id: id }, { where: { user_id: userData.id } });
-      break;
-    case 'nine_id':
-      // eslint-disable-next-line no-await-in-loop
-      await UserTimeTable.update({ nine_id: id }, { where: { user_id: userData.id } });
-      break;
-    default:
-      return res.json({
-        error: {
-          message: '시간표는 9개까지 추가가 가능합니다.',
-        },
-      });
-  }
+  await UserTimeTable.update(
+    {
+      [attributes[nullCount]]: id,
+    },
+    { where: { user_id: userData.id } },
+  );
+
   return res.json({
     data: {
       message: '정상적으로 추가되었습니다.',
+    },
+  });
+});
+
+timeTable.delete('/', async (req, res) => {
+  // eslint-disable-next-line prefer-destructuring
+  const { id } = req.body;
+  const token = req.header('x-access-token');
+  const userData = verify(token, process.env.JWT_SECRET);
+  const userTimeTables = await UserTimeTable.findOne({
+    attributes,
+    where: {
+      user_id: userData.id,
+    },
+    raw: true,
+  });
+  const nullCount = Object.values(userTimeTables).indexOf(id);
+
+  await UserTimeTable.update(
+    {
+      [attributes[nullCount]]: null,
+    },
+    { where: { user_id: userData.id } },
+  );
+
+  return res.json({
+    data: {
+      message: '정상적으로 삭제되었습니다.',
     },
   });
 });
